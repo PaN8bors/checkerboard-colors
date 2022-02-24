ea6a3823d9b531137c70e80284958eb9a6af98a4
Parish Neighbors <pn8bors@gmail.com>
2022-02-24 13:54:00 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 function main() {
   const myBoxes = document.getElementsByClassName('mybox');
   const myButton = document.getElementById('shuffle');
+  let i = 0;
 
   myButton.addEventListener('click', () => {
+    for (const myBox of myBoxes) {
+      if ((i % 2) === 0) {
+        myBox.style.backgroundColor = "green";
+      } else {
+        myBox.style.backgroundColor = "red";
+    }
+    i += 1;
+  }
   });
 }
